65f581dfaa3e95498a7b4d70b07dbaadf8bb5e84
DESKTOP-DJ5EJ84\wojte <wojtek.wacek.dola@gmail.com>
2023-07-12 16:42:29 +1000

    deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/voi-toi",
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/googlebooks",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
